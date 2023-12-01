980b807b53b5b523ab82543a56ab844a0af74a49
Sena Akbulut <Senakbulut@users.noreply.github.com>
2023-12-01 16:55:38 +0300

    google ads script added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
+import Script from "next/script";
 import { Layout } from "../components";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        id="adsbygoogle-init"
+        async
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1840036753551392"
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
